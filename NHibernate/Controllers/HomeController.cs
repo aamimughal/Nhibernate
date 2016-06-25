1cc1e5182b2bcff4688d000b4c9758199903fb4b
amina <aami_mughal@outlook.com>
2016-06-25 13:05:54 -0700

    controller

--- NHibernate/Controllers/HomeController.cs
+++ NHibernate/Controllers/HomeController.cs
@@ -18,6 +18,8 @@ namespace NHibernate.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+            
+         
         }
 
         public ActionResult Contact()
